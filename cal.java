3111f7a8f747454c89af3775ce92fa145747fe22
JUN-HYEOK SEO <starlights2283@gmail.com>
2023-06-26 13:56:12 +0900

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,16 @@
+package note;
+
+public class note2x {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1~9 xpi");
+		for(int x = 1; x < 10; x++) {
+			for(int y = 1; y < 10; y ++) {
+				System.out.println(x+"x"+y+"="+(x * y));
+			}
+		}
+
+	}
+
+}
